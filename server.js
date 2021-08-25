5a90f49aa0277a17eeb9d99e28a1629a006a2c37
Asmovic <asogba007@gmail.com>
2021-08-25 07:20:13 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+
+//Store all JS and CSS in Scripts folder.
+app.use(express.static(__dirname + '/public'));
+
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/views'));
+
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, console.log('Server listening on PORT ' + PORT));
+
