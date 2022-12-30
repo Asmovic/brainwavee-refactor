64759288d83aaa2f20da3ede0aa69c6e38c8e0b2
Asmovic <asogba007@gmail.com>
2022-12-30 15:13:01 +0100

    Remove view directory

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const path = require('path');
 
 const app = express();
 
-//Store all JS and CSS in Scripts folder.
+//Store all HTML, JS and CSS in Scripts folder.
 app.use(express.static(__dirname + '/public'));
 
-//Store all HTML files in view folder.
-app.use(express.static(__dirname + '/views'));
-
 app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname, './views'))
-})
+    res.sendFile(path.join(__dirname, './public'))
+});
+
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
